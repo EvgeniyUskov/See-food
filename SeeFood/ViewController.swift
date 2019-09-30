4918d3f627d42d9b7c60197768adc5fcffefc47b
Evgeniy Uskov <driftrby@gmail.com>
2019-09-30 10:22:30 +0700

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Evgeniy Uskov on 30/09/2019.
+//  Copyright © 2019 Evgeniy Uskov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
